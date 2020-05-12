dac6edc12443f5395cad9e3d37952f4da563bdca
BassemMagdi0007 <bassem.magdi00007@gmail.com>
2020-05-12 17:30:21 +0200

    seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'role' => 0,
+            'password'=> Hash::make('123456789'),
+        ]);
+        User::create([
+            'name' => 'employee',
+            'email' => 'employee@admin.com',
+            'role' => 1,
+            'password'=> Hash::make('123456789'),
+        ]);
+        User::create([
+            'name' => 'employee',
+            'email' => 'customer@employee.com',
+            'role' => 2,
+            'password'=> Hash::make('123456789'),
+        ]);
+    }
+}
